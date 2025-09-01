5d8842b90eb623824ff1aa41b852695d0bddb534
BangJu0620 <bju0620@naver.com>
2025-09-01 17:14:13 +0900

    Feat : 문, 레버 구현

--- Assets/Scripts/Structure/Trampolin.cs
+++ Assets/Scripts/Structure/Trampolin.cs
@@ -13,9 +13,7 @@ public class Trampolin : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
         // 플레이어인지 검사
-        //KinematicObj kineObj = collision.gameObject.TryGetComponent<KinematicObj>(out kineObj)
         if (collision.gameObject.TryGetComponent(out KinematicObj kineObj))
         {
             foreach (var contact in collision.contacts)
@@ -35,23 +33,5 @@ public class Trampolin : MonoBehaviour
             }
             isUpPos = false;
         }
-
-        //if (collision.gameObject.CompareTag("Player"))
-        //{
-        //    foreach (var contact in collision.contacts)
-        //    {
-        //        // 어디에서 충돌했는지 검사하기, 옆면이면 무시하기
-        //        if (Vector3.Dot(contact.normal, Vector3.up) < -0.995f)
-        //        {
-        //            isUpPos = true;
-        //            break;
-        //        }
-        //    }
-        //    if (isUpPos)
-        //    {
-        //        // 플레이어한테 점프 시키기
-        //        collision.rigidbody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-        //    }
-        //}
     }
 }
