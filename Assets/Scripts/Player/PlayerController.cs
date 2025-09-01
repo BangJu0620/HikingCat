988a2110890af4072936d61907ff6ea4c6ff5b44
C0mm4 <comonhuman@gmail.com>
2025-09-01 16:33:26 +0900

    Feat : 이동 시 점프 차징 불가, 점프 차징 시 이동 불가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -16,6 +16,8 @@ public class PlayerController : MonoBehaviour
     private float jumpChargeTime = 0f;
     [SerializeField] private float maxChargeTime = 1.2f;
 
+    private bool isMoving = false;
+
     private void Update()
     {
         physicsModel.InputHandler(curMovementInput * statusModel.Speed);
@@ -23,35 +25,43 @@ public class PlayerController : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        if (context.phase == InputActionPhase.Performed)
+        if (!isJumpCharge)
         {
-            curMovementInput = context.ReadValue<Vector2>();
-        }
-        else if (context.phase == InputActionPhase.Canceled)
-        {
-            curMovementInput = Vector2.zero;
+            if (context.phase == InputActionPhase.Performed)
+            {
+                isMoving = true;
+                curMovementInput = context.ReadValue<Vector2>();
+            }
+            else if (context.phase == InputActionPhase.Canceled)
+            {
+                isMoving = false;
+                curMovementInput = Vector2.zero;
+            }
         }
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        // 여기서 점프 차징 관련도 들어가긴 해야함.
-        if (context.phase == InputActionPhase.Started)
+        if (!isMoving)
         {
-            isJumpCharge = true;
-            jumpChargeTime = Time.time;
-        }
-        if(context.phase == InputActionPhase.Canceled)
-        {
-            isJumpCharge = false;
+            // 여기서 점프 차징 관련도 들어가긴 해야함.
+            if (context.phase == InputActionPhase.Started)
+            {
+                isJumpCharge = true;
+                jumpChargeTime = Time.time;
+            }
+            if (context.phase == InputActionPhase.Canceled)
+            {
+                isJumpCharge = false;
 
-            float heldTime = Time.time - jumpChargeTime;
+                float heldTime = Time.time - jumpChargeTime;
 
-            // 0~1로 정규화
-            float chargeRange = Mathf.Clamp01(heldTime / maxChargeTime);
+                // 0~1로 정규화
+                float chargeRange = Mathf.Clamp01(heldTime / maxChargeTime);
 
-            // 점프 실행
-            physicsModel.Jump(statusModel.GetJumpForce(chargeRange));
+                // 점프 실행
+                physicsModel.Jump(statusModel.GetJumpForce(chargeRange));
+            }
         }
     }
 }
