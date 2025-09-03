54cbcb4883506e36978ad3ab239716aa1390f90f
C0mm4 <comonhuman@gmail.com>
2025-09-03 14:19:47 +0900

    Refactor : 기존 매니저들 Singleton 패턴화

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -3,41 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
-    private static UIManager instance;
-    public static UIManager Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = FindObjectOfType<UIManager>();
-
-                if (instance == null)
-                {
-                    GameObject go = new GameObject("UIManager");
-                    instance = go.AddComponent<UIManager>();
-                    DontDestroyOnLoad(go);
-                }
-            }
-            return instance;
-        }
-    }
 
     public GameObject previousPanel = null;
-
-    private void Awake()
-    {
-        if (instance != null && instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
 
     public void Show(GameObject panel) => panel?.SetActive(true);
     public void Hide(GameObject panel) => panel?.SetActive(false);
