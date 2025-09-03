b0ef77400aa26f3a461bae4c0453e03a079b6aa5
Insung Park <sleepwithoutadream@gmail.com>
2025-09-03 11:13:26 +0900

    Feat: Sound: Tested bgm & sound sliders

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static SoundManager instance;
+    public static SoundManager Instance
     {
-        
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<SoundManager>();
+
+                if (instance == null)
+                {
+                    GameObject go = new GameObject("SoundManager");
+                    instance = go.AddComponent<SoundManager>();
+                    DontDestroyOnLoad(go);
+                }
+            }
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // [SerializeField] private AudioMixer mixer;        
+    // [SerializeField] private string bgmParam = "Test_BGM";
+    // [SerializeField] private string sfxParam = "Test_SFX";
+
+    private void Awake()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+
     }
 }
