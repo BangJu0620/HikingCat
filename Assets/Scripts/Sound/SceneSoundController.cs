8f5984b4d5e55db5d5a7e5c517e18b30de0b2c85
Insung Park <sleepwithoutadream@gmail.com>
2025-09-04 16:17:16 +0900

    Feat: Sound: Updated sound controller

--- Assets/Scripts/Sound/SceneSoundController.cs
+++ Assets/Scripts/Sound/SceneSoundController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TitleSceneSoundController : MonoBehaviour
+public class SceneSoundController : MonoBehaviour
 {
     [SerializeField] private AudioClip bgm;
 
